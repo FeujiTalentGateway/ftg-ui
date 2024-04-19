f2b7180983480cfecc01aec6e3ea26c5b3507555
Rakesh9890 <129718922+Rakesh9890@users.noreply.github.com>
2024-04-19 17:41:24 +0530

    Feature/ ftg 56 modify scheduling exam UI to include coding questions (#40)
    * FTG-59 : Inculded coding questions to exam form based on latest json
    
    * Common/sprint9 (#39)
    
    * change user icon
    
    change user icon
    
    * added test config for all the files in project
    
    * Added coding question component
    
    * added  coding questions,method and arguments
    
    * Modified register page html page by adding icons and logic for password and confirm password match (#36)
    
    * backend connect
    
    * added test cases stepper
    
    * added input arguments
    
    * FTG -49 : Created template for taking exam with coding questions
    
    * FTG -49 : Changed api url
    
    * Integrated with java api to save the coding question and also added some comments
    
    ---------
    
    Co-authored-by: samundar7 <144010958+samundar7@users.noreply.github.com>
    Co-authored-by: Aravind <144014129+Aravind9390@users.noreply.github.com>
    Co-authored-by: Dhamodhar <dhamodharam.kakkammagari@feuji.com>
    Co-authored-by: sivakrishnareddy826 <122780300+sivakrishnareddy826@users.noreply.github.com>
    Co-authored-by: Dhamodhar <144222784+Dhamodhar7190@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: samundar7 <144010958+samundar7@users.noreply.github.com>
    Co-authored-by: Aravind <144014129+Aravind9390@users.noreply.github.com>
    Co-authored-by: Dhamodhar <dhamodharam.kakkammagari@feuji.com>
    Co-authored-by: sivakrishnareddy826 <122780300+sivakrishnareddy826@users.noreply.github.com>
    Co-authored-by: Dhamodhar <144222784+Dhamodhar7190@users.noreply.github.com>

--- src/app/user/user-exam/questions/questions.component.ts
+++ src/app/user/user-exam/questions/questions.component.ts
@@ -31,7 +31,6 @@ export class QuestionsComponent implements OnInit, OnDestroy {
     created_at_ts: '',
     examSubjects: [],
     users: [],
-    codingQuestions:[]
   };
   codingSubjectName="Coding Questions"
   currentQuestionIndex = 0;

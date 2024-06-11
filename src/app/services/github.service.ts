1c032afcfa5ca27c05c133a82b32aada9569bda8
Ankush Raj <129718487+AnkushRaj0101@users.noreply.github.com>
2024-06-11 17:14:39 +0530

    Minor Change

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -32,7 +32,7 @@ export class GithubService {
   ) {}
 
   signInWithGitHub() {
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${environment.apiUrl+LOGIN_URL}&scope=${this.githubScope}`;
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=http://localhost:4200/main/login&scope=${this.githubScope}`;
   }
   handleGitHubCallback(code: string) {
     const requestBody = {
--- src/app/utils/CONSTANT/Api_constant.ts
+++ src/app/utils/CONSTANT/Api_constant.ts
@@ -1,5 +1,2 @@
-import { GITHUB_CLIENT_ID } from "./String_constant";
 
 export const GITHUB_REGISTRATION_URL = "/registration/github";
-export const LOGIN_URL = "/main/login";
-export const GITHUB_CODE_URL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}}&redirect_uri=${LOGIN_URL}&scope=user`;

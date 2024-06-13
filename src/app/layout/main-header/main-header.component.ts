e2eb1226bd419ca0ac1d805c742dfda003f21833
samundar7 <144010958+samundar7@users.noreply.github.com>
2024-06-13 16:27:58 +0530

    profile pic storing in local storage
    profile pic storing in local storage

--- src/app/layout/main-header/main-header.component.ts
+++ src/app/layout/main-header/main-header.component.ts
@@ -23,7 +23,7 @@ export class MainHeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.userName = localStorage.getItem('userName') as string;
-    const profileUrl = sessionStorage.getItem('profilePictureUrl') as string
+    const profileUrl = localStorage.getItem('profilePictureUrl') as string
     
     if (profileUrl) {
       this.profilePictureUrl = this.domSanitizer.bypassSecurityTrustUrl(profileUrl);

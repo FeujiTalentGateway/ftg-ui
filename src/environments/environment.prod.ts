fcc863cd1aef88e08390de9b41c204ea61f8f963
samundar7 <144010958+samundar7@users.noreply.github.com>
2024-06-22 23:56:19 +0530

    change enviorment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  apiUrl: 'http://35.154.116.10:8092/',
-  adminUrl: 'http://35.154.116.10:8093/',
-  pythonUrl: 'http://35.154.116.10:8000/',
-  uiUrl:'http://35.154.116.10:4200/',
-  authTokenKey: 'prod-token',
+  apiUrl: 'http://localhost:8092/',
+  adminUrl: 'http://localhost:8093/',
+  pythonUrl: 'http://localhost:8000/',
+  uiUrl:'http://localhost:4200/',
+  authTokenKey: 'dev-token',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,7 @@
 export const environment = {
-  apiUrl: 'http://localhost:8092/', // Development URL
-  // apiUrl: 'http://35.154.116.10:8092/', // Production URL
-
-  adminUrl: 'http://localhost:8093/', // Development URL
-  // adminUrl: 'http://35.154.116.10:8093/', // Production URL
-
-  pythonUrl: 'http://localhost:8000/', // Development URL
-  //pythonUrl: 'http://35.154.116.10:8000/', // Production URL
-
-  uiUrl:'http://localhost:4200', // Development URL
-  //uiUrl: 'http://35.154.116.10:4200/', // Production URL
+  apiUrl: 'http://localhost:8092/',
+  adminUrl: 'http://localhost:8093/',
+  pythonUrl: 'http://localhost:8000/',
+  uiUrl:'http://localhost:4200/',
   authTokenKey: 'dev-token',
 };

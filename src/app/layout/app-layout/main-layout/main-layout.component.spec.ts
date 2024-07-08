3e056f17db182859dd65c7f13af63ccad5cf5cbe
Ankush Raj <129718487+AnkushRaj0101@users.noreply.github.com>
2024-07-08 16:36:22 +0530

    mainLayout Component testcases

--- src/app/layout/app-layout/main-layout/main-layout.component.spec.ts
+++ src/app/layout/app-layout/main-layout/main-layout.component.spec.ts
@@ -1,33 +1,79 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+import { UserdetailsService } from 'src/app/services/userdetails.service';
 import { MainLayoutComponent } from './main-layout.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MatDialogModule } from '@angular/material/dialog';
-import { SidebarComponent } from '../../sidebar/sidebar.component';
-import { MainHeaderComponent } from '../../main-header/main-header.component';
-import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from '../../sidebar/sidebar.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MainHeaderComponent } from '../../main-header/main-header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MainLayoutComponent', () => {
   let component: MainLayoutComponent;
   let fixture: ComponentFixture<MainLayoutComponent>;
+  let userDetailsSpy: jasmine.SpyObj<UserdetailsService>;
+  let matDrawerSpy: jasmine.SpyObj<MatDrawer>;
+  let matDialog: jasmine.SpyObj<MatDialog>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [MainLayoutComponent,SidebarComponent,MainHeaderComponent,MatDrawerContainer,MatDrawer,MatDrawerContent],
-      imports:[HttpClientTestingModule,MatSnackBarModule,FormsModule,ReactiveFormsModule,RouterTestingModule,MatDialogModule,BrowserAnimationsModule]
+  beforeEach(async () => {
+    const userDetailsServiceSpy = jasmine.createSpyObj('UserdetailsService', ['getUserNameFromToken']);
 
+    await TestBed.configureTestingModule({
+      declarations: [MainLayoutComponent, SidebarComponent, MainHeaderComponent],
+      imports: [
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: UserdetailsService, useValue: userDetailsServiceSpy },
+        { provide: MatDialog, useValue: jasmine.createSpyObj('MatDialog', ['open']) },
+        { provide: MatSnackBar, useValue: jasmine.createSpyObj('MatSnackBar', ['open']) },
 
-    });
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MainLayoutComponent);
     component = fixture.componentInstance;
+    userDetailsSpy = TestBed.inject(UserdetailsService) as jasmine.SpyObj<UserdetailsService>;
+    matDrawerSpy = jasmine.createSpyObj('MatDrawer', ['toggle']);
+    matDialog = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>; // Inject MatDialog mock
+
+    userDetailsSpy.getUserNameFromToken.and.returnValue(of('John Doe'));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have a defined component', () => {
+    expect(component).toBeDefined();
+  });
+  it('should initialize user data', () => {
+    component.ngOnInit();
+
+    expect(component.userData).toBeDefined();
+
+    expect(component.userData.userName).toBeUndefined();
+  });
+
+  it('should initialize userName$ observable with user name from service', () => {
+    component.ngOnInit();
+    expect(component.userName$).toBeDefined();
+    component.userName$.subscribe(userName => {
+      expect(userName).toBe('John Doe');
+    });
+  });
+
+  it('should toggle the sidenav drawer', () => {
+    component.drawer = matDrawerSpy;
+    component.toggleLayout();
+    expect(matDrawerSpy.toggle).toHaveBeenCalled();
   });
 });

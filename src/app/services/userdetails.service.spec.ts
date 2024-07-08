3698c5e481e844145ccab008663eeec9c12bdaf4
Ankush Raj <129718487+AnkushRaj0101@users.noreply.github.com>
2024-07-08 11:46:03 +0530

    Added UserDetailed Service

--- src/app/services/userdetails.service.spec.ts
+++ src/app/services/userdetails.service.spec.ts
@@ -1,22 +1,47 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { UserdetailsService } from './userdetails.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-describe('Service: Userdetails', () => {
+describe('UserdetailsService', () => {
+  let service: UserdetailsService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule],
+      providers: [UserdetailsService]
+    });
+    service = TestBed.inject(UserdetailsService);
+  });
 
-      providers: [UserdetailsService],
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should set and get user name', (done: DoneFn) => {
+    const testName = 'John Doe';
+    service.setNameFromToken(testName);
+
+    service.getNameFromToken().subscribe(name => {
+      expect(name).toBe(testName);
+      done();
     });
   });
 
-  it('should ...', inject(
-    [UserdetailsService],
-    (service: UserdetailsService) => {
-      expect(service).toBeTruthy();
-    }
-  ));
+  it('should set and get user roles', (done: DoneFn) => {
+    const testRoles = ['admin', 'user'];
+    service.setRoleFromToken(testRoles);
+
+    service.getRoleFromToken().subscribe(roles => {
+      expect(roles).toEqual(testRoles);
+      done();
+    });
+  });
+
+  it('should set and get user username', (done: DoneFn) => {
+    const testUserName = 'johndoe';
+    service.setUserNameFromToken(testUserName);
+
+    service.getUserNameFromToken().subscribe(userName => {
+      expect(userName).toBe(testUserName);
+      done();
+    });
+  });
 });
